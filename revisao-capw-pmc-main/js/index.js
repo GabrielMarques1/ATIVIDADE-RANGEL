61b17e49d5dff54b4022efe183c8c842678a8bff
Edimar Gabriel Marques <139916604+GabrielMarques1@users.noreply.github.com>
2024-04-12 01:53:46 -0300

    Add files via upload

---
+++ revisao-capw-pmc-main/js/index.js
@@ -0,0 +1,33 @@
+let indiceAtual = 0;
+const secoes 
+        = document.querySelectorAll("section");
+const btnAvancar 
+        = document.getElementById("avancar");
+const btnVoltar 
+        = document.getElementById("voltar");
+
+function rolarParaSecao(indice){
+    if(indice >= 0 && indice < secoes.length){
+        secoes[indice]
+            .scrollIntoView({ behavior: "smooth"});
+        indiceAtual = indice;
+    }
+}
+
+btnAvancar.addEventListener("click", function(){
+    rolarParaSecao(indiceAtual + 1);
+})
+
+btnVoltar.addEventListener("click", function(){
+    rolarParaSecao(indiceAtual - 1);
+})
+
+
+const botoesLikes 
+    = document.querySelectorAll('.like');
+
+botoesLikes.forEach(btn => {
+    btn.addEventListener("click", function(){
+        this.classList.toggle("curti")
+    });
+});
